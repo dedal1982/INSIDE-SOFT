bfb4d6fd131568f92352be094035d8833584e65c
aldm <d44.a1@yandex.ru>
2024-09-09 23:18:47 +0300

    1

---
+++ js/heath.js
@@ -0,0 +1,40 @@
+// const WHCSGO = {
+//   imgFull:
+//     "https://avatars.mds.yandex.net/i?id=dc3d95d3be1bc91f25e17c83df292bf3a5deffaf-7753204-images-thumbs&n=13",
+//   mini1:
+//     "https://avatars.mds.yandex.net/i?id=dc3d95d3be1bc91f25e17c83df292bf3a5deffaf-7753204-images-thumbs&n=13",
+//   mini2:
+//     "https://avatars.mds.yandex.net/i?id=dc3d95d3be1bc91f25e17c83df292bf3a5deffaf-7753204-images-thumbs&n=13",
+//   mini3:
+//     "https://avatars.mds.yandex.net/i?id=dc3d95d3be1bc91f25e17c83df292bf3a5deffaf-7753204-images-thumbs&n=13",
+//   mini4:
+//     "https://avatars.mds.yandex.net/i?id=dc3d95d3be1bc91f25e17c83df292bf3a5deffaf-7753204-images-thumbs&n=13",
+// };
+
+// function displayImages() {
+
+//   const imgFull = document.querySelector(".shop__images-full");
+//   const mini1 = document.querySelector(".mini-1");
+//   const mini2 = document.querySelector(".mini-2");
+//   const mini3 = document.querySelector(".mini-3");
+//   const mini4 = document.querySelector(".mini-4");
+
+//   imgFull.src = WHCSGO.imgFull;
+//   imgFull.alt = "Full image";
+
+//   mini1.src = WHCSGO.mini1;
+//   mini1.alt = "Mini image 1";
+
+//   mini2.src = WHCSGO.mini2;
+//   mini2.alt = "Mini image 2";
+
+//   mini3.src = WHCSGO.mini3;
+//   mini3.alt = "Mini image 3";
+
+//   mini4.src = WHCSGO.mini4;
+//   mini4.alt = "Mini image 4";
+// }
+
+// const clickableElement = document.querySelector(".some-clickable-element");
+
+// clickableElement.addEventListener("click", displayImages);
--- js/init-cards-cheat-CSGO.js
+++ js/init-cards-cheat-CSGO.js
@@ -3,32 +3,36 @@ const initialCardCSGO = [
   {
     image:
       "https://avatars.mds.yandex.net/i?id=dc3d95d3be1bc91f25e17c83df292bf3a5deffaf-7753204-images-thumbs&n=13",
-    name: "CS GO",
+    name: "AIM",
     price: "от 2000 ₽",
     props: "true",
     detect: "UNDETECTED",
     color: "#00ff38",
+    id: "id-1",
   },
   {
     image:
       "https://avatars.mds.yandex.net/i?id=dc3d95d3be1bc91f25e17c83df292bf3a5deffaf-7753204-images-thumbs&n=13",
-    name: "CS GO",
+    name: "WH",
     price: "от 3000 ₽",
     props: "true",
     detect: "DETECTED",
     color: "red",
+    id: "id-2",
   },
   {
     image:
       "https://avatars.mds.yandex.net/i?id=dc3d95d3be1bc91f25e17c83df292bf3a5deffaf-7753204-images-thumbs&n=13",
-    name: "CS GO",
+    name: "AIM",
     price: "от 4000 ₽",
+    id: "id-3",
   },
   {
     image:
       "https://avatars.mds.yandex.net/i?id=dc3d95d3be1bc91f25e17c83df292bf3a5deffaf-7753204-images-thumbs&n=13",
-    name: "CS GO",
+    name: "WH",
     price: "от 5000 ₽",
+    id: "id-4",
   },
 ];
 
@@ -39,12 +43,15 @@ const elements = document.querySelector(".cheaths__wrapper");
 
 function createCard(item) {
   const htmlElement = itemTemplate.cloneNode(true);
+  const idCheat = htmlElement.querySelector("#idCheat");
   const nameCheat = htmlElement.querySelector(".name");
   const gameImage = htmlElement.querySelector(".cheaths__image");
   const gamePrice = htmlElement.querySelector(".price");
   const cheathDetect = htmlElement.querySelector(".detect");
 
-  // Проверяем наличие значения поля в объекте item перед присваиванием его элементу
+  if (item.id) {
+    idCheat.id = item.id;
+  }
   if (item.name) {
     nameCheat.textContent = item.name;
   }
